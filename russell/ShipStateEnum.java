a0e163358d984f8c5dc663fb492deef073f65a44
Russell Bryan <bryanr@ou.edu>
2016-02-19 16:36:03 -0600

    shouldn't have lips macros anymore.

--- russell/ShipStateEnum.java
+++ russell/ShipStateEnum.java
@@ -1,9 +1,8 @@
-~(load "macros.scm")
 
 package brya3525;
 
 public enum ShipStateEnum {
-	GATHERING_ENERGY,
-	GATHERING_RESOURCES,
-	DELIVERING_RESOURCES
+    GATHERING_ENERGY,
+    GATHERING_RESOURCES,
+    DELIVERING_RESOURCES
 }
--- russell/build.sh
+++ russell/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 for i in $(find -name '*.java'); do
 	echo "Lips processing: " $i
-	lips < $i | astyle > ../src/brya3525/$(basename $i)
+	lips < $i | astyle > output/$(basename $i)
+	
 done
-cd ..
-ant spacesettlers-human
