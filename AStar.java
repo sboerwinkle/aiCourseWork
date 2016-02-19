a7c8cefaa9e25fb0aff75bca29757be293b5b83c
Simon Boerwinkle <sboerwinkle@gmail.com>
2016-02-19 16:31:13 -0600

    Think I finished A*, but it hasn't been tested yet.

--- AStar.java
+++ AStar.java
@@ -4,8 +4,19 @@ import spacesettlers.objects.*;
 import spacesettlers.simulator.Toroidal2DPhysics;
 import spacesettlers.utilities.Position;
 import spacesettlers.utilities.Vector2D;
+import java.util.PriorityQueue;
 
 class AStar {
+	static final double SQRT2 = Math.sqrt(2);
+
+	static final int[] dxs = {-1, -1, -1,  0,  1,  1,  1,  0};
+	static final int[] dys = {-1,  0,  1,  1,  1,  0, -1, -1};
+	static final double S2 = 1.4142;
+	static final double[] dists = {S2, 1, S2, 1, S2, 1, S2, 1};
+	static final double V1 = Path.CRUISE_SPEED;
+	static final double V2 = V1/S2;
+	static final double[] vxs = {-V2, -V1, -V2,   0,  V2,  V1,  V2,   0};
+	static final double[] vxs = {-V2,   0,  V2,  V1,  V2,   0, -V2, -V1};
 
 	//These variables are used to keep from passing a dozen different values around internally.
 	//I never said this class was thread-safe, and it isn't.
@@ -19,11 +30,10 @@ class AStar {
 
 	/**
 	 * Performs A* algorithm on a rotated grid of points.
-	 * Throws a NoPathException if it can't find a way through.
-	 * @return a Path object containing the desired path.
+	 * @return a Path object containing the desired path, or <code>null</code> if no such path exists
 	 * @param a The object from which to pull the starting position. Assumed that you'll start here, stationary.
 	 * @param b The object to get to. Assumed we're going to match its velocity.
-	 * @param me The ship who's going to be doing this. The only object besides a and b which the collision checker ignores. Also uses my radius in calculations, not a's radius, since we're assuming a is actually stationary.
+	 * @param me The ship who's going to be doing this. The only object besides a and b which the collision checker ignores. Also used for radius and mass of navigating object.
 	 */
 	static Path doAStar(Toroidal2DPhysics space, AbstractObject a, AbstractObject b, Ship me) {
 		radius = me.getRadius();
@@ -31,39 +41,110 @@ class AStar {
 		origin = a;
 		destination = b;
 		destVel = b.getPosition().getTranslationalVelocity();
+		Vector2D destPos = new Vector2D(b.getPosition());
 
-		//Something about a priority queue
-			//Needs a score, obvs.
-			//Which node it is
-			//Who its parent is
-		//Initialize queue to a's location.
-			//Score is just the heuristic
-			//Node is what it is
-			//Parent is, liek, -1.
-		//Forever
-			//Is priority queue empty?
-				//Fuck
-				//throw new NoPathException();
-			//Grab top element of priority queue
-				//Is goal?
-					//Fuck yeah
-					//break
-				//Am I visited?
-					//Continue
-				//Mark me as visited
-				//For each cardinal direction
-					//If result invalid or visited, continue
-					//Compute step
-					//Is step not okay?
-						//continue
-					//Add a node to priority queue
-		return new Path(new Vector2D[0], b.getId(), 0, 0);
+		Vector2D offset = space.findShortestDistanceVector(destination.getPosition(), origin.getPosition());
+		double dist = offset.getMagnitude();
+		int steps = Math.cieling(dist/(radius*2));
+		double timePer = dist/steps/V1;
+		Vector2D vecNorm = offset.divide(steps);
+		Vector2D vecTan = new Vector2D(vecNorm.getYValue(), -vecNorm.getXValue());
+		int halfWidth = (steps+2)/4;
+		int width = 2*halfWidth+1;
+		int numNodes = width*(steps+1);
+		int startNode = numNodes-1-halfWidth;
+		int goalNode = halfWidth;
+		int[] parents = new int[numNodes];
+		boolean[] visited = new boolean[numNodes];
+		PriorityQueue<Node> q = new PriorityQueue<Node>();
+		q.add(new Node(startNode, -1, 0, steps*timePer));
+		Node top;
+		while (true) {
+			top = q.poll();
+			if (top == null) return null;
+			int ix = top.ix;
+			if (visited[ix]) continue;
+			parents[ix] = top.parent;
+			if (ix = goalNode) break;
+			visited[ix] = true;
+			int n = ix / width;
+			int t = ix % width - halfWidth;
+			for (int dir = 0; dir < 8; dir++) {
+				int N = n+dxs[dir];
+				int T = t+dys[dir];
+				if (math.abs(T) > halfWidth || N < 0 || N > steps) continue;
+				if (visited[N*width+T+halfWidth]) continue;
+				if (!stepOkay(space, new Step(top.g, dists[dir]*timePer, new Position(destPos.add(vecNorm.multiply(n)).add(vecTan.multiply(t))), new Vector2D(vxs[dir], vys[dir])))) continue;
+				//Set things up so N holds displacement along the longer axis, and T along the shorter axis
+				T = Math.abs(T);
+				if (T > N) {
+					int tmp = T;
+					T = N;
+					N = tmp;
+				}
+				//Change it so N is now axis-aligned displacement, and T is 45-degree displacement
+				N -= T;
+				//Use these values to compute h(n) for the new node
+				q.add(new Node(N*width+T+halfWidth, ix, top.g+dists[dir]*timePer, (N+S2*T)*timePer));
+			}
+		}
+		//Once over to compute number of nodes in path
+		//Start at -1 because we do't want to count the end as a ndoe
+		numNodes = -1;
+		for (int n = goalNode; n != startNode; n = parents[n]) numNodes++;
+
+		Vector2D[] waypoints = new Vector2D[numNodes];
+		//Add the path elements to the vector,
+		//Computing energy consumption as we go
+		double ecost = Toroidal2DPhysics.ENERGY_PENALTY*me.getMass()*Path.CRUISE_SPEED;
+		double energy = ecost; // This covers the initial acceleration from assumed standstill
+		Vector2D prev = null;
+		int prevN = 0;
+		int prevT = 0;
+		for (int n = parents[goalNode]; n != startNode; n = parents[n]) {
+			int N = n/width;
+			int T = n%width - halfWidth;
+			Vector2D direction = new Vector2D(N-prevN, T-prevT);
+			//If we have displacement along both axes, divide by sqrt(2) to make it a unit vector.
+			if (((N-prevN)&(T-prevT)) == 1) direction.divide(S2);
+			if (prev != null) {
+				energy += ecost*direction.subtract(prev).getMagnitude();
+			}
+			prev = direction;
+			prevN = N;
+			prevT = T;
+			waypoints[--numNodes] = vecNorm.multiply(N).add(vecTan.multiply(T));
+		}
+		return new Path(new Vector2D[0], b.getId(), energy, top.g);
+	}
+
+	static class Node implements Comparable<Node> {
+		int ix;
+		int parent;
+		double g, h;
+		public int compareTo(Node other) {
+			f = g+h;
+			o = other.g+other.h;
+			if (f < o) return -1;
+			if (f > o) return 1;
+			return 0;
+		}
+		Node(int i, int p, double G, double H) {
+			ix = i;
+			parent = p;
+			g = G;
+			h = H;
+		}
 	}
 
 	static class Step {
+		//From now
 		double startTime;
+		//From startTime
 		double duration;
+		//Global
 		Vector2D startPos;
+		//Relative to destination
 		Vector2D velocity;
 		Step(double t, double dt, Vector2D p, Vector2D v) {
 			startTime = t;
