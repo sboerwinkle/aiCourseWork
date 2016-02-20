fa8091298648daf3e36d73b3aa7e75f7a2b15ea9
EN Barnett <en@ou.edu>
2016-02-20 12:35:25 -0600

    rep Two incorporated into rep One

--- KnowledgeRepOne.java
+++ KnowledgeRepOne.java
@@ -23,6 +23,10 @@ import spacesettlers.utilities.Vector2D;
 import spacesettlers.clients.TeamClient;
 
 class KnowledgeRepOne {
+	
+	private static final int NEAR_BEACON_RADIUS = 50;
+	private static final int LOW_ENERGY = 1000;
+	
 	/**
 	 * This will either be a mineable asteroid, or the base.
 	 * This is a UUID, so it's persistent.
@@ -40,6 +44,9 @@ class KnowledgeRepOne {
 		return path.getGraphics();
 	}
 
+	/**
+	 * Counter for when to do replans
+	 */
 	int timeTilAStar = 0;
 
 	/**
@@ -55,7 +62,7 @@ class KnowledgeRepOne {
 	 * Finds the home base corresponding to my ship
 	 * @return The home base for our team, or null if none is found
 	 */
-	AbstractObject getNearestBase(Toroidal2DPhysics space, Ship me) {
+	Base getNearestBase(Toroidal2DPhysics space, Ship me) {
 		double nearestDist = Double.MAX_VALUE;
 		Base nearestBase = null;
 		for (Base b : space.getBases()) {
@@ -68,6 +75,89 @@ class KnowledgeRepOne {
 		return nearestBase;
 	}
 
+	/**
+	 * Finds the mineable asteroid closest to the ship
+	 * @param space
+	 * @param me
+	 * @return The mineable asteroid nearest to my ship, or null if there are none
+	 */
+	Asteroid getNearestAsteroid(Toroidal2DPhysics space, Ship me) {
+		Asteroid besteroid = null;
+		double bestDist = Double.MAX_VALUE;
+		for (Asteroid a : space.getAsteroids()) {
+			//It must be mineable
+			if (!a.isMineable()) continue;
+			//and close to me
+			double dist = space.findShortestDistance(a.getPosition(), me.getPosition());
+			if (dist > bestDist) continue;
+
+			besteroid = a;
+			bestDist = dist;
+		}
+		return besteroid;
+	}
+	
+	/**
+	 * Find the nearest beacon
+	 * @param space
+	 * @param me
+	 * @return The beacon nearest to my ship, or null if there are none
+	 */
+	Beacon getNearestBeacon(Toroidal2DPhysics space, Ship me) {
+		double bestDist = Double.MAX_VALUE;
+		Beacon bestbeacon = null;
+		for (Beacon b : space.getBeacons()){
+			double dist = space.findShortestDistance(b.getPosition(), me.getPosition());
+			if (dist > bestDist) continue;
+			
+			bestbeacon = b;
+			bestDist = dist;
+		}
+		return bestbeacon;
+	}
+	
+	/**
+	 * Check if there is a beacon within a specified radius
+	 * @param space
+	 * @param ship
+	 * @return true if there is at least one beacon in our radius, false otherwise
+	 */
+	boolean isBeaconNear(Toroidal2DPhysics space, Ship me) {
+		for (Beacon b : space.getBeacons()){
+			if (space.findShortestDistance(b.getPosition(), me.getPosition()) < NEAR_BEACON_RADIUS) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Find the closest way to refuel, be it a base or an energy beacon, assuming the base has enough energy in it
+	 * @param space
+	 * @param me
+	 * @return the base or beacon that is closest
+	 */
+	AbstractObject myNearestRefuel(Toroidal2DPhysics space, Ship me) {
+		Beacon closestBeacon = getNearestBeacon(space, me);
+		Base home = getNearestBase(space, me); 
+		if (home.getEnergy() > LOW_ENERGY
+				&& space.findShortestDistance(home.getPosition(), me.getPosition()) < space.findShortestDistance(closestBeacon.getPosition(), me.getPosition())) {
+			return home;
+		}
+		else {
+			return closestBeacon;
+		}
+	}
+	
+	/**
+	 * Detect if we are running low on energy
+	 * @param myShip
+	 * @return true if ship energy is less than threshold
+	 */
+	boolean isEnergyLow(Ship me){
+		return (me.getEnergy() < LOW_ENERGY);
+	}
+	
 	/**
 	 * Updates our objective and target.
 	 * Should be called once per tick.
@@ -89,19 +179,8 @@ class KnowledgeRepOne {
 		//If he have no resources (And aren't already hunting an asteroid), pick an asteroid to hunt.
 		if ((objective == null || objective instanceof Base) && me.getResources().getMass() == 0) {
 			timeTilAStar = 0;
-			Asteroid besteroid = null;
-			double bestDist = Double.MAX_VALUE;
-			for (Asteroid a : space.getAsteroids()) {
-				//It must be mineable
-				if (!a.isMineable()) continue;
-				//and close to me
-				double dist = space.findShortestDistance(a.getPosition(), me.getPosition());
-				if (dist > bestDist) continue;
 
-				besteroid = a;
-				bestDist = dist;
-			}
-			objective = besteroid;
+			objective = getNearestAsteroid(space, me);
 			objectiveID = objective == null ? null : objective.getId();
 			if (objective == null) return;
 		}
