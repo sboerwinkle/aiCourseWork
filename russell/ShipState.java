be87326f651c38539f5888466954a1e901ce18d3
EN Barnett <en@ou.edu>
2016-03-23 19:07:04 -0500

    ship state with chromosome

--- russell/ShipState.java
+++ russell/ShipState.java
@@ -1,41 +1,9 @@
 
 package barn1474.russell;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
-import java.awt.Color;
-import barn1474.russell.TextGraphics;
-import barn1474.russell.Knowledge;
-import barn1474.russell.Prescience;
-import barn1474.russell.SpaceSimulation;
-import barn1474.russell.LibrePD;
-import barn1474.russell.ShipState;
-import barn1474.russell.ShipStateEnum;
-import spacesettlers.actions.AbstractAction;
-import spacesettlers.actions.DoNothingAction;
-import spacesettlers.actions.MoveAction;
-import spacesettlers.actions.RawAction;
-import spacesettlers.actions.PurchaseTypes;
-import spacesettlers.actions.PurchaseCosts;
-import spacesettlers.graphics.CircleGraphics;
-import spacesettlers.graphics.SpacewarGraphics;
-import spacesettlers.objects.AbstractActionableObject;
-import spacesettlers.objects.AbstractObject;
 import spacesettlers.objects.Ship;
-import spacesettlers.objects.powerups.SpaceSettlersPowerupEnum;
-import spacesettlers.objects.resources.ResourcePile;
-import spacesettlers.objects.weapons.AbstractWeapon;
-import spacesettlers.simulator.Toroidal2DPhysics;
 import spacesettlers.utilities.Position;
-import spacesettlers.utilities.Vector2D;
+import barn1474.evolution.BbbIndividual;
 
 class ShipState {
     Ship ship;
@@ -43,6 +11,8 @@ class ShipState {
     ShipStateEnum state;
     boolean shooting;
     long lastShotTick;
+    //holds chromosome and fitness for this ship
+    BbbIndividual chromosome;
 
 
     public ShipState(Ship ship,Position aimPoint) {
